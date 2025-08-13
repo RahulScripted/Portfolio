27a2b9aa6b901ed851e57329538ec372b981bd43
Rahul Goswami <rahul.goswami@mintifi.com>
2025-08-13 15:02:12 +0530

    JUst frame added

--- src/Pages/Achievement.jsx
+++ src/Pages/Achievement.jsx
@@ -1,9 +1,20 @@
+import { motion } from 'framer-motion';
+import Cards from '../components/Cards';
 
 
 const Achievement = () => {
   return (
-    <div className="flex items-center justify-center w-full min-h-[70vh]">
-        <h1>Coming Soon</h1>
+    <div className='flex item-center justify-center flex-col-reverse lg:justify-between lg:flex-row p-5 lg:p-10 gap-10'>
+
+      {/* Left Part */}
+      <motion.div 
+        className='relative w-full lg:sticky lg:top-10 lg:w-1/2 lg:h-screen'
+        initial={{ x: -100, opacity: 0 }} 
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1, type: "spring", stiffness: 50 }}
+      >
+        <Cards />
+      </motion.div>
     </div>
   )
 }
